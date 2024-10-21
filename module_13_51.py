215cedee905554e78f0d6447c11f5fec83295445
IlkaevEvgeny <Chaoscpr@mail.ru>
2024-10-21 18:49:49 +0500

    Add files via upload

---
+++ module_13_51.py
@@ -0,0 +1,71 @@
+from aiogram import Bot, Dispatcher,executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+import asyncio
+
+
+
+api = ''
+
+bot = Bot(token=api)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage = MemoryStorage())
+
+# Создание клавиатуры
+keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+button_calculate = types.KeyboardButton('Рассчитать')
+button_info = types.KeyboardButton('Информация')
+keyboard.add(button_calculate, button_info)
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+# Обработка команды start
+@dp.message_handler(commands=['start'])
+async def start_command(message):
+    await message.answer("Привет!", reply_markup=keyboard)
+
+
+# Обработка текста 'Рассчитать'
+@dp.message_handler(text='Рассчитать')
+async def set_age(message):
+    await message.answer('Введите ваш возраст:')# запрос age
+    await UserState.age.set()
+
+
+@dp.message_handler(state=UserState.age) #хендлер обработка ввода age.
+async def set_growth(message, state):
+    await state.update_data(age=int(message.text))#запись введеного age, в формате init.
+    await message.answer('Введите ваш рост (в сантиметрах):')# запрос growth
+    await UserState.next()
+
+
+@dp.message_handler(state=UserState.growth)#хендлер обработка ввода growth.
+async def set_weight(message, state):
+    await state.update_data(growth=int(message.text))#запись введеного growth, в формате init.
+    await message.answer('Введите ваш вес (в килограммах):')# запрос weight
+    await UserState.next()
+
+
+@dp.message_handler(state=UserState.weight) #хендлер обработка ввода weight.
+async def send_calories(message, state):
+    await state.update_data(weight=int(message.text))#запись введеного weight, в формате init.
+    data = await state.get_data() # словарь введенных данных
+
+    #получим данные из словаря
+    age = data['age']
+    growth = data['growth']
+    weight = data['weight']
+
+
+    calories = 10 * weight + 6.25 * growth - 5 * age + 5 # Рассчитаем калории.
+
+    await message.answer(f"Ваша норма калорий: {calories:.0f} ккал")
+    await state.finish()
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
